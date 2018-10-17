3c11cc01d54b74bb78764c18ec9ffc27b531ff2c
Claire Tran <ctran@expert360.com>
2018-10-17 21:18:14 +1100

    adding logic

--- lib/sweeper.ex
+++ lib/sweeper.ex
@@ -1,3 +1,20 @@
 defmodule Sweeper do
-	
+  def find_match(event, events) do
+    IO.inspect event
+
+    sort(events)
+    |> Enum.map(&intersect(event, &1))
+    |> IO.inspect
+  end
+
+  defp sort(events) do
+    result = Enum.sort_by(events, fn event -> event.start_date end)
+    IO.inspect result
+  end
+
+  defp intersect(e1, e2) do
+    (e1.start_date > e2.start_date && e1.start_date < e2.end_date) ||
+    (e1.start_date > e2.start_date && e1.start_date < e2.start_date) ||
+      (e1.end_date > e2.start_date && e1.end_date <= e2.end_date)
+  end
 end
--- test/sweeper_test.exs
+++ test/sweeper_test.exs
@@ -2,7 +2,25 @@ defmodule SweeperTest do
   use ExUnit.Case
   doctest Sweeper
 
-  test "..." do
-    
+  test "new event end date overlaps with existing event" do
+    assert Sweeper.find_match(event(10, 15), [event(12, 17)]) == [true]
+  end
+
+  test "new event does not overlap existing event" do
+    assert Sweeper.find_match(event(10, 15), [event(1, 7)]) == [false]
+    assert Sweeper.find_match(event(10, 15), [event(3, 10)]) == [false]
+    assert Sweeper.find_match(event(10, 15), [event(15, 18)]) == [false]
+  end
+
+  test "new event start date overlaps existing event" do
+    assert Sweeper.find_match(event(10, 15), [event(8, 11)]) == [true]
+  end
+
+  test "new event exactly overlaps existing event" do
+    assert Sweeper.find_match(event(10, 15), [event(10, 15)]) == [true]
+  end
+
+  defp event(start_date, end_date) do
+    %{start_date: start_date, end_date: end_date}
   end
 end
