2f3b9923ae1271d3ff73a3e6039be7ef1ab82230
Claire Tran <ctran@expert360.com>
2018-10-18 14:22:22 +1100

    Updating logic to handle event types

--- lib/sweeper.ex
+++ lib/sweeper.ex
@@ -1,31 +1,57 @@
 defmodule Sweeper do
   def find_match(event, events) do
-    IO.inspect event
-
-    sort(events)
-    |> Enum.map(&intersect?(event, &1))
-    |> IO.inspect
+    # TODO: this could be refactored
+    result = Enum.map(sort(events), &edit_event(event, &1))
+    if event.type != :available do
+      Enum.uniq(List.insert_at(result, -1, event)) |> Enum.reject(&is_nil/1)
+    else
+      Enum.reject(result, &is_nil/1)
+    end
   end
 
   defp sort(events) do
-    result = Enum.sort_by(events, fn event -> event.start_date end)
-    IO.inspect result
+    Enum.sort_by(events, fn event -> event.start_date end)
   end
 
-  defp intersect?(%{start_date: s1, end_date: e1}, %{start_date: s2, end_date: e2})
+  # NOTE: Hack to insert the new event. In real use, insertion/deletion/update would occur at the time of finding intersecting events.
+#  defp insert_new_event(result, event = %{start_date: s, end_date: e, type: t}) when t != :available do
+#    Enum.uniq(List.insert_at(result, -1, event))
+#  end
+#  defp insert_new_event(_, result), do: result
+
+  defp edit_event(event1 = %{start_date: s1, end_date: e1, type: t1}, %{start_date: s2, end_date: e2})
+       when (s1 == s2 and e1 == e2) do
+    cond do
+      t1 == :available -> nil # delete existing event e2
+      true -> event1 # :unavailable or :part_time, save or edit existing record to update the type
+    end
+  end
+
+  defp edit_event(%{start_date: s1, type: t1}, %{start_date: s2, end_date: e2, type: t2})
     when (s1 > s2 and s1 < e2) do
-    true
+    cond do
+      t1 == t2 ->
+        %{start_date: s2, end_date: s1, type: t1}
+      t1 == :available ->
+        %{start_date: s2, end_date: s1, type: t2}
+      t1 == :unavailable ->
+        %{start_date: s2, end_date: s1, type: t2}
+      t1 == :part_time ->
+        %{start_date: s2, end_date: s1, type: t2}
+      true ->
+        %{start_date: s2, end_date: e2, type: t2} # no change
+    end
   end
 
-  defp intersect?(%{start_date: s1, end_date: e1}, %{start_date: s2, end_date: e2})
+  defp edit_event(%{start_date: s1}, %{start_date: s2})
     when (s1 > s2 and s1 < s2) do
     true
   end
 
-  defp intersect?(%{start_date: s1, end_date: e1}, %{start_date: s2, end_date: e2})
+  defp edit_event(%{end_date: e1}, %{start_date: s2, end_date: e2})
     when (e1 > s2 and e1 <= e2) do
     true
   end
 
-  defp intersect?(_, _), do: false
+  defp edit_event(_, _), do: false
 end
--- test/sweeper_test.exs
+++ test/sweeper_test.exs
@@ -2,22 +2,57 @@ defmodule SweeperTest do
   use ExUnit.Case
   doctest Sweeper
 
-  test "new event end date overlaps with existing event" do
-    assert Sweeper.find_match(event(10, 15, :unavailable), [event(12, 17, :unavailable)]) == [true]
+#  test "new event end date overlaps with existing event" do
+#    result = Sweeper.find_match(event(10, 15, :unavailable), [event(12, 17, :unavailable)])
+#    assert {:ok, event(15, 17, :unavailable)} == result
+#  end
+
+#  test "new event does not overlap existing event" do
+#    assert Sweeper.find_match(event(10, 15, :unavailable), [event(1, 7, :unavailable)]) == [false]
+#    assert Sweeper.find_match(event(10, 15, :unavailable), [event(3, 10, :unavailable)]) == [false]
+#    assert Sweeper.find_match(event(10, 15, :unavailable), [event(15, 18, :unavailable)]) == [false]
+#  end
+
+  describe "new event overlaps existing event" do
+    test "new event start date overlaps existing event of same type" do
+      result = Sweeper.find_match(event(10, 15, :unavailable), [event(8, 11, :unavailable)])
+      assert result == [event(8, 10, :unavailable), event(10, 15, :unavailable)]
+    end
+
+    test "available event start date overlaps an existing unavailable event" do
+      result = Sweeper.find_match(event(10, 15, :available), [event(8, 11, :unavailable)])
+      assert result == [event(8, 10, :unavailable)]
+    end
+
+    test "available event start date overlaps an existing part time event" do
+      result = Sweeper.find_match(event(10, 15, :available), [event(8, 11, :part_time)])
+      assert result == [event(8, 10, :part_time)]
+    end
+
+    test "unavailable event overlaps a part time event" do
+      result = Sweeper.find_match(event(10, 15, :unavailable), [event(8, 11, :part_time)])
+      assert result == [event(8, 10, :part_time), event(10, 15, :unavailable)]
+    end
+
+    test "part time event overlaps an unavailable event" do
+      result = Sweeper.find_match(event(10, 15, :part_time), [event(8, 11, :unavailable)])
+      assert result == [event(8, 10, :unavailable), event(10, 15, :part_time)]
+    end
   end
 
-  test "new event does not overlap existing event" do
-    assert Sweeper.find_match(event(10, 15, :unavailable), [event(1, 7, :unavailable)]) == [false]
-    assert Sweeper.find_match(event(10, 15, :unavailable), [event(3, 10, :unavailable)]) == [false]
-    assert Sweeper.find_match(event(10, 15, :unavailable), [event(15, 18, :unavailable)]) == [false]
-  end
-
-  test "new event start date overlaps existing event" do
-    assert Sweeper.find_match(event(10, 15, :unavailable), [event(8, 11, :unavailable)]) == [true]
-  end
-
-  test "new event exactly overlaps existing event" do
-    assert Sweeper.find_match(event(10, 15, :unavailable), [event(10, 15, :unavailable)]) == [true]
+  describe "new event exactly overlaps existing event" do
+    test "new event is the same type" do
+      assert Sweeper.find_match(event(10, 15, :unavailable), [event(10, 15, :unavailable)]) == [event(10, 15, :unavailable)]
+    end
+    test "new event is available" do
+      assert Sweeper.find_match(event(10, 15, :available), [event(10, 15, :unavailable)]) == []
+    end
+    test "new event is unavailable" do
+      assert Sweeper.find_match(event(10, 15, :unavailable), [event(10, 15, :part_time)]) == [event(10, 15, :unavailable)]
+    end
+    test "new event is part time" do
+      assert Sweeper.find_match(event(10, 15, :part_time), [event(10, 15, :unavailable)]) == [event(10, 15, :part_time)]
+    end
   end
 
   defp event(start_date, end_date, type) do
